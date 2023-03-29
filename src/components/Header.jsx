ada94e2765d83e8094077efd13cf413bb145a74d
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-29 17:04:57 +0200

    header side bar complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import { useState } from "react";
 
 function Header() {
+  const [burgerStatus, setBurgerStatus] = useState(false);
+
   return (
     <Container>
       <a>
@@ -17,8 +21,47 @@ function Header() {
       <RightMenu>
         <a href="#">Shop</a>
         <a href="#">Tesla Account</a>
-        <CustomMenu></CustomMenu>
+        <CustomMenu onClick={() => setBurgerStatus(true)} />
       </RightMenu>
+
+      <BurgerNav show={burgerStatus}>
+        <CloseWrapper>
+          <CustomClose onClick={() => setBurgerStatus(false)} />
+        </CloseWrapper>
+        <li>
+          <a href="#">Existing Inventory</a>
+        </li>
+        <li>
+          <a href="#">Used Inventory</a>
+        </li>
+        <li>
+          <a href="#">Trade-in</a>
+        </li>
+        <li>
+          <a href="#">Cybertruck</a>
+        </li>
+        <li>
+          <a href="#">Roadster</a>
+        </li>
+        <li>
+          <a href="#">Semi</a>
+        </li>
+        <li>
+          <a href="#">Charging</a>
+        </li>
+        <li>
+          <a href="#">Power</a>
+        </li>
+        <li>
+          <a href="#">Computer</a>
+        </li>
+        <li>
+          <a href="#">Utilities</a>
+        </li>
+        <li>
+          <a href="#">Test Drive</a>
+        </li>
+      </BurgerNav>
     </Container>
   );
 }
@@ -35,6 +78,7 @@ const Container = styled.div`
   left: 0;
   right: 0;
   align-items: center;
+  z-index: 100;
 `;
 const Menu = styled.div`
   display: flex;
@@ -65,4 +109,34 @@ const RightMenu = styled.div`
 
 const CustomMenu = styled(MenuIcon)`
   cursor: pointer;
+`;
+
+const BurgerNav = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  background: white;
+  width: 300px;
+  padding: 20px;
+  flex-direction: column;
+  transition: transform 0.3s ease-in-out;
+  transform: ${(props) => (props.show ? "translateX(0)" : "translate(100%)")};
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+    a {
+      font-weight: 600;
+    }
+  }
+`;
+
+const CustomClose = styled(CloseIcon)`
+  cursor: pointer;
+`;
+
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
 `;
